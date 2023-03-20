0757619f45555c5041af435f09b2145bc91e60ef
ebenezerolusheun <128372410+ebenezerolusheun@users.noreply.github.com>
2023-03-20 11:56:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-Omega 3 & Blueberry Oatmeal
+Omega '3' & Blueberry Oatmeal
 
 Kale, Spinach & Rocket Smoothie
 
