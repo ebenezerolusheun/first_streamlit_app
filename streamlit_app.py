6ac9f8485fbb3a97754c7810bdd8c7d577c92720
ebenezerolusheun <128372410+ebenezerolusheun@users.noreply.github.com>
2023-03-20 12:36:07 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 ## using pandas to read data from the S3 bucket 
 my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+
+my_fruit_list = my_fruits_list.set_index('Fruit')
 
 ## Let's put a pic list here so they can pic the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
