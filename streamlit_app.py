129b1f259f8fe724f3a20cd47227ad44ab3f18fe
ebenezerolusheun <128372410+ebenezerolusheun@users.noreply.github.com>
2023-03-21 13:04:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ fruityvice_response = read_api('kiwi')
 ##fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
-streamlit.text(fruityvice_response.json())
+##streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
-## fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # write your own comment - what does this do?
-## streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
