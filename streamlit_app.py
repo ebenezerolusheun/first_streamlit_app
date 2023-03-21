484bfae1b8dbb03d2a9765064da829c1280f8dfb
ebenezerolusheun <128372410+ebenezerolusheun@users.noreply.github.com>
2023-03-21 12:55:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,12 @@ streamlit.dataframe(fruits_to_show)
 ## New Section to display fruityvice api response
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-def read_api(value:str):
-  return requests.get(f"https://fruityvice.com/api/fruit/{value}")
-fruityvice_response = read_api('watermelon')
+## def read_api(value:str):
+  ## return requests.get(f"https://fruityvice.com/api/fruit/{value}")
+## fruityvice_response = read_api('watermelon')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 
 ### streamlit.text(fruityvice_response.json())
 
