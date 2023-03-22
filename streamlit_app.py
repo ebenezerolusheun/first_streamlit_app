937c7e15c1a210b750dd6d803352b0d7c8278b8e
ebenezerolusheun <128372410+ebenezerolusheun@users.noreply.github.com>
2023-03-22 11:21:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,10 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 # Let streamlite to display the object as dataframe
 st.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
